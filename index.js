98153d603ae3202a1c884a549fc69925f91f1046
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-14 19:50:58 +0600

    connect with ema-zon client side

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vuk2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('emaZon').collection('product');
+
+
+        // get all products
+        app.get('/product', async(req,res)=>{
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if(page || size){
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                 products = await cursor.toArray();
+            }
+         
+            res.send(products);
+        })
+
+        // pagination
+        app.get('/productCount', async(req,res)=>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count});
+        })
+
+        // Post to get products by ids
+        app.post('/productByKeys', async(req,res)=>{
+            const keys = req.body;
+             console.log(keys);
+            const ids = keys.map(id=> ObjectId(id));
+            const query = {_id: {$in: ids}}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
+           
+        })
+      
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req,res)=>{
+    res.send('ema-zon is running');
+});
+
+app.listen(port, ()=>{
+    console.log('ema-zon is running', port);
+})
